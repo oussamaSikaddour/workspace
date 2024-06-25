a1e18f8a7acd2f34e323775702240470e6768379
oussama sikaddour <34127545+oussamaSikaddour@users.noreply.github.com>
2024-06-25 20:34:14 +0200

    cc

--- resources/js/traits/IntersectionObserver.js
+++ resources/js/traits/IntersectionObserver.js
@@ -1,7 +1,7 @@
 export const inView = (element, visibilityPercentage, callback, props = {}) => {
-//   if (!element) {
-//     return;
-//   }
+  if (!element) {
+    return;
+  }
 
   const observerOptions = {
     threshold: visibilityPercentage / 100,
@@ -11,8 +11,7 @@ export const inView = (element, visibilityPercentage, callback, props = {}) => {
 
 
   const observer = new IntersectionObserver((entries)=>{
-    console.log(entries[0])
-    callback(entries[0]?.isIntersecting,props)
+    callback(entries.isIntersecting,props)
   }, observerOptions);
 
 
